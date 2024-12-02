fbbec1846dad6c01d3a5f2612ada08e43ce33df2
rwinkah <rwinosa@gmail.com>
2024-12-02 12:45:53 +0000

    fix: update createeliveryORDER

--- src/index.js
+++ src/index.js
@@ -245,7 +245,7 @@ app.post("/shopify/webhooks/order/create", async (req, res) => {
 
 		console.log("CREATING ORDER");
 		// console.log(deliveryFee);
-		const feeId = await deliveryFee.response.data.feeId;
+		const feeId = await deliveryFee.response.feeId;
 		const locationDetails =
 			await shopifyIntegration.getInventoryLocationDetails();
 
